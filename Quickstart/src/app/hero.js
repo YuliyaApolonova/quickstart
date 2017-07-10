c716a1d81a424728b3e96c0e9a2e97a694ada62f
Juliya <yuliyaapl@mail.ru>
2017-07-10 19:16:27 +0300

    Add all

---
+++ Quickstart/src/app/hero.js
@@ -0,0 +1,11 @@
+"use strict";
+/**
+ * Created by user on 10.07.17.
+ */
+var Hero = (function () {
+    function Hero() {
+    }
+    return Hero;
+}());
+exports.Hero = Hero;
+//# sourceMappingURL=hero.js.map
